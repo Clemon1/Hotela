ecdfc7ad5fd8e66b4f7f624103f5d19fc3cae6b0
brsekani <lawaltemidayo06@gmail.com>
2024-07-20 15:16:03 +0100

    push

--- hotela-fe/src/features/dashboard/HostelLovedByGuest.jsx
+++ hotela-fe/src/features/dashboard/HostelLovedByGuest.jsx
@@ -1,7 +1,80 @@
-import { Box, Flex, Group, Image, Stack, Text } from "@mantine/core";
+import { Box, Image, Stack, Text, Flex } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
 import dashBoardRoom1 from "../../assets/dashBoardRoom1.jpg";
 import { FaAngleRight } from "react-icons/fa";
-import { Carousel } from "@mantine/carousel";
+
+const items = [
+  {
+    id: 1,
+    name: "Holo Hotel",
+    location: "Islington",
+    price: "from $100/night",
+    image: dashBoardRoom1,
+  },
+  {
+    id: 2,
+    name: "Holo Hotel",
+    location: "Islington",
+    price: "from $120/night",
+    image: dashBoardRoom1,
+  },
+  {
+    id: 3,
+    name: "Holo Hotel",
+    location: "Islington",
+    price: "from $130/night",
+    image: dashBoardRoom1,
+  },
+  {
+    id: 4,
+    name: "Holo Hotel",
+    location: "Islington",
+    price: "from $140/night",
+    image: dashBoardRoom1,
+  },
+  {
+    id: 5,
+    name: "Holo Hotel",
+    location: "Islington",
+    price: "from $150/night",
+    image: dashBoardRoom1,
+  },
+  {
+    id: 6,
+    name: "Holo Hotel",
+    location: "Islington",
+    price: "from $160/night",
+    image: dashBoardRoom1,
+  },
+  {
+    id: 7,
+    name: "Holo Hotel",
+    location: "Islington",
+    price: "from $170/night",
+    image: dashBoardRoom1,
+  },
+  {
+    id: 8,
+    name: "Holo Hotel",
+    location: "Islington",
+    price: "from $180/night",
+    image: dashBoardRoom1,
+  },
+  {
+    id: 9,
+    name: "Holo Hotel",
+    location: "Islington",
+    price: "from $190/night",
+    image: dashBoardRoom1,
+  },
+  {
+    id: 10,
+    name: "Holo Hotel",
+    location: "Islington",
+    price: "from $200/night",
+    image: dashBoardRoom1,
+  },
+];
 
 function HostelLovedByGuest() {
   return (
@@ -10,36 +83,48 @@ function HostelLovedByGuest() {
         Hostel Loved By Guest
       </Text>
 
-      <Carousel>
-        <Stack
-          w={250}
-          h={250}
-          p={5}
-          gap={0}
-          style={{
-            boxShadow: `0px 4px 8px rgba(0, 0, 0, 0.4)`,
-            borderRadius: "15px",
-            alignItems: "flex-start", // Align items to the start
-            display: "flex", // Ensure flex display is used
-          }}
-        >
-          <Box style={{ borderRadius: "15px", overflow: "hidden" }}>
-            <Image radius={15} src={dashBoardRoom1} />
-          </Box>
+      <Carousel
+        withIndicators
+        height={300}
+        slideSize="20%"
+        slideGap="md"
+        loop
+        align="start"
+        controlSize={32}
+      >
+        {items.map((item) => (
+          <Carousel.Slide key={item.id}>
+            <Stack
+              w={250}
+              h={250}
+              p={5}
+              gap={0}
+              style={{
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.4)",
+                borderRadius: "15px",
+                alignItems: "flex-start",
+                display: "flex",
+              }}
+            >
+              <Box style={{ borderRadius: "15px", overflow: "hidden" }}>
+                <Image radius={15} src={item.image} />
+              </Box>
 
-          <Box>
-            <Text>Holo Hotel</Text>
-            <Text>Islington</Text>
-          </Box>
+              <Box>
+                <Text>{item.name}</Text>
+                <Text>{item.location}</Text>
+              </Box>
 
-          <Flex
-            align="center"
-            style={{ width: "100%", justifyContent: "space-between" }}
-          >
-            <Text>from $130/night</Text>
-            <FaAngleRight size={20} />
-          </Flex>
-        </Stack>
+              <Flex
+                align="center"
+                style={{ width: "100%", justifyContent: "space-between" }}
+              >
+                <Text>{item.price}</Text>
+                <FaAngleRight size={20} />
+              </Flex>
+            </Stack>
+          </Carousel.Slide>
+        ))}
       </Carousel>
     </Box>
   );
